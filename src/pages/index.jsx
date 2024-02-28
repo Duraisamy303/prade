28fca2a09f7c9f45a7bc8e0b74481b050c3c58be
Duraisamy <duraisamy.p@sanorac.com>
2024-02-28 14:40:40 +0530

    change index screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,40 +1,38 @@
-import SEO from "@/components/seo";
-import Wrapper from "@/layout/wrapper";
-import Header from "@/layout/headers/header";
-import ElectronicCategory from "@/components/categories/electronic-category";
-import HomeHeroSlider from "@/components/hero-banner/home-hero-slider";
-import FeatureArea from "@/components/features/feature-area";
-import ProductArea from "@/components/products/electronics/product-area";
-import BannerArea from "@/components/banner/banner-area";
-import OfferProducts from "@/components/products/electronics/offer-products";
-import ProductGadgetArea from "@/components/products/electronics/product-gadget-area";
-import ProductBanner from "@/components/products/electronics/product-banner";
-import ProductSmArea from "@/components/products/electronics/product-sm-area";
-import NewArrivals from "@/components/products/electronics/new-arrivals";
-import BlogArea from "@/components/blog/electronic/blog-area";
-import InstagramArea from "@/components/instagram/instagram-area";
-import CtaArea from "@/components/cta/cta-area";
-import Footer from "@/layout/footers/footer";
+import React from 'react';
+import SEO from '@/components/seo';
+import Wrapper from '@/layout/wrapper';
+import HeaderFour from '@/layout/headers/header-4';
+import JewelryBanner from '@/components/banner/jewelry-banner';
+import JewelryShopBanner from '@/components/shop-banner/jewelry-shop-banner';
+import JewelryAbout from '@/components/about/jewelry-about';
+import PopularProducts from '@/components/products/jewelry/popular-products';
+import ProductArea from '@/components/products/jewelry/product-area';
+import JewelryCollectionBanner from '@/components/shop-banner/jewelry-collection-banner';
+import BestSellerPrd from '@/components/products/jewelry/best-seller-prd';
+import JewelryBrands from '@/components/brand/jewelry-brands';
+import InstagramAreaFour from '@/components/instagram/instagram-area-4';
+import FeatureAreaThree from '@/components/features/feature-area-3';
+import FooterTwo from '@/layout/footers/footer-2';
+import HeaderTwo from '@/layout/headers/header-2';
 
-export default function Home() {
+const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle='Home'/>
-      <Header/>
-      <HomeHeroSlider/>
-      <ElectronicCategory/>
-      <FeatureArea/>
-      <ProductArea/>
-      <BannerArea/>
-      <OfferProducts/>
-      <ProductGadgetArea/>
-      <ProductBanner/>
-       <NewArrivals/>
-      <ProductSmArea/>
-      <BlogArea/>
-      <InstagramArea/>
-      <CtaArea/>
-      <Footer/>
+      <SEO pageTitle="Home Four" />
+      <HeaderTwo/>
+      <JewelryBanner/>
+      <FeatureAreaThree />
+      <JewelryShopBanner/>
+      {/* <JewelryAbout/> */}
+      <PopularProducts/>
+      {/* <ProductArea/> */}
+      <JewelryCollectionBanner/>
+      {/* <BestSellerPrd/> */}
+      {/* <JewelryBrands/> */}
+      <InstagramAreaFour/>
+      <FooterTwo/>
     </Wrapper>
-  )
-}
+  );
+};
+
+export default index;
